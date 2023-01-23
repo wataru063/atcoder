a8ca65278026db4bdad87022e5dfae8b8b702505
skgch0702 <skgch0702@gmail.com>
2023-01-23 11:40:15 +0900

    add ruby-ABC257-B

---
+++ ruby/ABC257/B.rb
@@ -0,0 +1,12 @@
+n, k, q = gets.split.map(&:to_i)
+as = gets.chomp.split.map(&:to_i)
+ls = gets.chomp.split.map(&:to_i)
+
+q.times do |num|
+  target = ls[num] - 1
+  next if (as[target] && as[target] >= n) || as.include?(as[target]+1)
+  as[target] = as[target] + 1
+end
+puts as.join(" ")
+
+
